d8a06c7a1df80c554978431224132cf56f265a78
Hayden Hughes <mrhaydenhughes@gmail.com>
2017-04-30 17:36:58 +1000

    Update gui.py

--- airootfs/etc/skel/gui.py
+++ airootfs/etc/skel/gui.py
@@ -15,19 +15,19 @@ def internet():
 
 
 def shutdown():
-    os.system("shutdown")
+    os.system("shutdown now")
 
 
 root = Tk()
 label = Label(root, text="System Menu:", bg="white")
 label.pack()
-snislauncherbutton = Button(root, text="SNIS", command=snislauncher, bg="white")
+snislauncherbutton = Button(root, text="Space Nerds in Space", command=snislauncher, bg="white")
 snislauncherbutton.pack()
-soundbutton = Button(root, text="Sound", command=sound, bg="white")
+soundbutton = Button(root, text="Audio Settings", command=sound, bg="white")
 soundbutton.pack()
-internetbutton = Button(root, text="Connect to a network", command=internet, bg="white")
+internetbutton = Button(root, text="Internet Settings", command=internet, bg="white")
 internetbutton.pack()
-shutdownbutton = Button(root, text="Shutdown PC (in one minute)", command=shutdown, bg="white")
+shutdownbutton = Button(root, text="Shutdown PC", command=shutdown, bg="white")
 shutdownbutton.pack()
 root.configure(background="white")
 
