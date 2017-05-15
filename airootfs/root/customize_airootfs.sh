62966ca95965bc6be5151b4520383a2762988343
Hayden Hughes <mrhaydenhughes@gmail.com>
2017-05-15 16:58:50 +1000

    Add files via upload
    Add speech to text

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -23,4 +23,29 @@ systemctl enable pacman-init.service choose-mirror.service
 systemctl set-default multi-user.target
 systemctl enable NetworkManager.service
 
-! id user &&  useradd -m -p "" -g users  -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash user
+# ! id user &&  useradd -m -p "" -g users  -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash user
+
+cd /
+git clone https://github.com/smcameron/space-nerds-in-space.git 
+git clone https://github.com/smcameron/space-nerds-in-space-assets.git 
+cp -R space-nerds-in-space-assets/share/snis/solarsystems/* space-nerds-in-space/share/snis/solarsystems/
+cd space-nerds-in-space
+make
+cd ..
+rm -rf space-nerds-in-space-assets
+
+git clone https://github.com/cmusphinx/sphinxbase.git
+cd sphinxbase
+sh ./autogen.sh
+sh ./configure --enable-fixed
+make
+make install
+cd ..
+
+git clone https://github.com/cmusphinx/pocketsphinx.git
+cd pocketsphinx
+sh ./autogen.sh
+sh ./configure
+make
+make install
+cd ..
