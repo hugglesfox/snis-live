c3adfbaac52784dd324823e2fae305a84992aa55
Hayden Hughes <haydenhughes@protonmail.com>
2017-11-20 14:57:28 +1100

    Add wireless AP and ethernet DHCP functionality

--- airootfs/root/gui.py
+++ airootfs/root/gui.py
@@ -1,15 +1,34 @@
 from tkinter import *
-import os
+import subprocess
 
 
 def snislauncher():
-    os.system("xterm -e 'cd /space-nerds-in-space && ./snis_launcher'")
+    subprocess.call(["xterm -e 'cd /space-nerds-in-space && ./snis_launcher'"])
  
 
 def snisspeech():
-    os.system("xterm -e 'cd /space-nerds-in-space/speech && ./queeg500'")
+    subprocess.call(["xterm -e 'cd /space-nerds-in-space/speech && ./queeg500'"])
+
+def wireless():
+    try:
+        subprocess.call(["echo 'interface = $(((ls -1 /sys/class/net | grep '\<w.*\>') | head -1))' >> /etc/hostapd/hostapd.conf"])
+    except OSError:
+        win = tk.Toplevel()
+        win.wm_title("Window")
+
+        l = tk.Label(win, text="Error, No wireless adapters found")
+        l.grid(row=0, column=0)
+
+        b = ttk.Button(win, text="Okay", command=win.destroy)
+        b.grid(row=1, column=0)
+    else:
+        subprocess.call(["systemctl start dnsmasq.service && systemctl start hostapd.service"])
     
-    
+
+def dhcp():
+    subprocess.call(["systemctl start dnsmasq.service"])
+
+
 def sound():
     os.system("pavucontrol")
 
@@ -25,7 +44,7 @@ def shutdown():
 root = Tk()
 label = Label(root, text="System Menu:", bg="white")
 label.pack()
-snislauncherbutton = Button(root, text="Space Nerds in Space", command=snislauncher, bg="white")
+snislauncherbutton = Button(root, text="Start Space Nerds in Space", command=snislauncher, bg="white")
 snislauncherbutton.pack()
 speechbutton = Button(root, text="SNIS Speech", command=snisspeech, bg="white")
 speechbutton.pack()
@@ -33,6 +52,10 @@ soundbutton = Button(root, text="Audio Settings", command=sound, bg="white")
 soundbutton.pack()
 internetbutton = Button(root, text="Setup Wifi", command=internet, bg="white")
 internetbutton.pack()
+createapbutton = Button(root, text="Create Wireless Access Point", command=wireless, bg="white")
+createapbutton.pack()
+startdhcp = Button(root, text="Start DHCP Server on Ethernet", command=dhcp bg="white")
+startdhcp.pack()
 shutdownbutton = Button(root, text="Shutdown PC", command=shutdown, bg="white")
 shutdownbutton.pack()
 root.configure(background="white")
