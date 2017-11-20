c866e4b1d04f3891cd1e0d58a0651d6c73f4645e
Hayden Hughes <haydenhughes@protonmail.com>
2017-11-20 16:20:40 +1100

    Use optimized space nerds in space build option

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -28,7 +28,7 @@ git clone https://github.com/smcameron/space-nerds-in-space.git
 git clone https://github.com/smcameron/space-nerds-in-space-assets.git 
 cp -R space-nerds-in-space-assets/share/snis/solarsystems/* space-nerds-in-space/share/snis/solarsystems/
 cd space-nerds-in-space
-make
+make 0=1
 cd ..
 rm -rf space-nerds-in-space-assets
 
