7b134117a1042dc22dd50aafaa4655d1e3af45b5
Hayden Hughes <haydenhughes@protonmail.com>
2017-11-21 12:28:52 +1100

    Improve UX
    Add colors, fonts and popup boxes to gui.py

--- airootfs/root/gui.py
+++ airootfs/root/gui.py
@@ -1,11 +1,23 @@
-from tkinter import Tk, Label, Button
+from tkinter import Tk, Label, Button, Toplevel
 import os
+
+def popup(text):
+    toplevel = Toplevel()
+    toplevel.configure(background="black")
+    toplevel.title("Notice")
+
+    l = Label(toplevel, text=str(text), font=("Lucida Console", 16), fg="lime green", bg="black")
+    l.pack()
+
+    b = Button(toplevel, text="Close", command=toplevel.destroy, font=("Lucida Console", 12), fg="lime green", bg="black")
+    b.pack()
+    
+    toplevel.mainloop()
 
 def dhcp(interface):
     os.system("ifconfig " + str(interface) + " 10.0.0.1")
     os.system("ifconfg " + str(interface) + " netmask 255.255.255.0")
     os.system("systemctl start dnsmasq.service")
-
 
 def snislauncher():
     os.system("xterm -e 'cd /space-nerds-in-space && ./snis_launcher'")
@@ -17,9 +29,11 @@ def wireless():
     os.system("echo 'interface='$(ls -1 /sys/class/net | grep '\<w.*\>' | head -1) >> /etc/hostapd/hostapd.conf")
     dhcp("$(ls -1 /sys/class/net | grep '\<w.*\>' | head -1)")
     os.system("systemctl start hostapd.service")
+    popup("Started DHCP server")
 
 def dhcpeth():
     dhcp("$(ls -1 /sys/class/net | grep '\<e.*\>' | head -1)")
+    popup("Started DHCP server")
 
 def sound():
     os.system("pavucontrol")
@@ -31,22 +45,31 @@ def shutdown():
     os.system("systemctl poweroff")
 
 root = Tk()
-label = Label(root, text="System Menu:", bg="white")
+root.title("Menu")
+root.configure(background="black")
+
+label = Label(root, text="System Menu", font=("Lucida Console", 20), fg="lime green", bg="black")
 label.pack()
-snislauncherbutton = Button(root, text="Start Space Nerds in Space", command=snislauncher, bg="white")
+
+snislauncherbutton = Button(root, text="Start Space Nerds in Space", command=snislauncher, font=("Lucida Console", 12), fg="lime green", bg="black")
 snislauncherbutton.pack()
-speechbutton = Button(root, text="SNIS Speech", command=snisspeech, bg="white")
+
+speechbutton = Button(root, text="SNIS Speech", command=snisspeech, font=("Lucida Console", 12), fg="lime green", bg="black")
 speechbutton.pack()
-soundbutton = Button(root, text="Audio Settings", command=sound, bg="white")
+
+soundbutton = Button(root, text="Audio Settings", command=sound, font=("Lucida Console", 12), fg="lime green", bg="black")
 soundbutton.pack()
-internetbutton = Button(root, text="Setup Wifi", command=internet, bg="white")
+
+internetbutton = Button(root, text="Setup Wifi", command=internet, font=("Lucida Console", 12), fg="lime green", bg="black")
 internetbutton.pack()
-createapbutton = Button(root, text="Create Wireless Access Point", command=wireless, bg="white")
+
+createapbutton = Button(root, text="Create Wireless Access Point", command=wireless, font=("Lucida Console", 12), fg="lime green", bg="black")
 createapbutton.pack()
-startdhcp = Button(root, text="Start DHCP Server on Ethernet", command=dhcpeth, bg="white")
+
+startdhcp = Button(root, text="Start DHCP Server on Ethernet", command=dhcpeth, font=("Lucida Console", 12), fg="lime green", bg="black")
 startdhcp.pack()
-shutdownbutton = Button(root, text="Shutdown PC", command=shutdown, bg="white")
+
+shutdownbutton = Button(root, text="Shutdown PC", command=shutdown, font=("Lucida Console", 12), fg="lime green", bg="black")
 shutdownbutton.pack()
-root.configure(background="white")
 
 root.mainloop()
